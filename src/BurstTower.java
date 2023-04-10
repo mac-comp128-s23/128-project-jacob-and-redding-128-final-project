85bdfbce72763230094bc4e256cab1baf8abc547
ReddSaut <osauter@macalester.edu>
2023-04-10 11:04:03 -0500

    hierarchies

---
+++ src/BurstTower.java
@@ -0,0 +1,14 @@
+public class BurstTower implements Tower {
+
+    @Override
+    public double getRadius() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getRadius'");
+    }
+
+    @Override
+    public double getPrice() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPrice'");
+    }
+}
