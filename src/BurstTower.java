3ae4e7f216d5a85d1ae965bd7d67dc162e461b31
ReddSaut <osauter@macalester.edu>
2023-04-21 15:04:48 -0500

    Tower drag interface

--- src/BurstTower.java
+++ src/BurstTower.java
@@ -1,6 +1,6 @@
 import edu.macalester.graphics.*;
 
-public class BurstTower extends GraphicsGroup implements Tower {
+public class BurstTower extends Tower {
     private double price = 500, radius = 25;
     private Image base = new Image("Tower.png");
     private Image gun = new Image("Cannon.png");
@@ -13,15 +13,21 @@ public class BurstTower extends GraphicsGroup implements Tower {
         setCenter(x, y);
         add(base);
         add(gun);
-        
-        System.out.println("Gun anchor: " + gun.getAnchor());
-        System.out.println("Gun center: " + gun.getCenter());
 
+    }
+    public BurstTower(Tower copy) {
+        gun.setMaxHeight(radius * 3);
+        base.setMaxHeight(radius * 2);
+        gun.setCenter(0, 0);
+        base.setCenter(0, 0);
+        setCenter(copy.getCenter());
+        add(base);
+        add(gun);
     }
 
     public Runnable getRunningAnimation() {
         return (()-> {
-            gun.rotateBy(5);
+            //gun.rotateBy(5);
         });
     }
 }
